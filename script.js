b30bd56dfb55811c193ab413d7913b9b578bea6c
dudeable <129113631+dudeable@users.noreply.github.com>
2023-06-01 19:02:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const numberContainer = document.getElementById('number-container');
-  let score = 0;
+  const circleContainer = document.getElementById('circle-container');
+  const resetButton = document.getElementById('reset-button');
 
-  function createNumber() {
-    const number = document.createElement('div');
-    number.classList.add('number');
-    number.textContent = getRandomNumber();
+  const colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF'];
 
-    number.addEventListener('click', function() {
-      if (parseInt(number.textContent) === score + 1) {
-        score++;
-        number.style.backgroundColor = getRandomColor();
-        number.style.color = '#ffffff';
-        number.style.cursor = 'default';
-        number.removeEventListener('click', arguments.callee);
-      }
-      checkWinCondition();
+  function createCircle() {
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    circle.style.backgroundColor = randomColor;
+
+    circle.addEventListener('click', function() {
+      circle.remove();
     });
 
-    return number;
-  }
-
-  function getRandomNumber() {
-    return Math.floor(Math.random() * 10) + 1;
-  }
-
-  function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  function checkWinCondition() {
-    const numbers = numberContainer.getElementsByClassName('number');
-    if (score === numbers.length) {
-      setTimeout(function() {
-        alert('Congratulations! You clicked all the numbers!');
-        resetGame();
-      }, 200);
-    }
+    return circle;
   }
 
   function resetGame() {
-    score = 0;
-    numberContainer.innerHTML = '';
+    circleContainer.innerHTML = '';
 
-    for (let i = 0; i < 10; i++) {
-      const number = createNumber();
-      number
+    for (let i = 0; i < 20; i++) {
+      const circle = createCircle();
+      circleContainer.appendChild(circle);
+    }
+  }
+
+  resetButton.addEventListener('click', resetGame);
+
+  resetGame();
+});
+
