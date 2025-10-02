20f0d7804ca2b14de5c3645cdb7c72e9212c7800
dudeable <129113631+dudeable@users.noreply.github.com>
2025-10-02 10:28:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (circles.length === 0) {
       clearInterval(timerInterval);
       setTimeout(function() {
-        const playerName = prompt('Congratulations! You are supercalifragilistic!\nEnter your name for the leaderboard:');
+        const playerName = prompt('Congratulations! You are okay at doing work!\nEnter your name for the leaderboard:');
         if (playerName) {
           addToLeaderboard(playerName, score);
           updateLeaderboard();
